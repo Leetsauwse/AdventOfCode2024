2bc0d2212afb8ff49a41b701ba7846c308754ef8
Nate Axcell <nateaxcell@gmail.com>
2024-12-03 16:52:42 -0500

    Create file_utils.ts

---
+++ utils/file_utils.ts
@@ -0,0 +1,23 @@
+/**
+ * Reads puzzle input from a file and returns it as a string
+ */
+export async function readInput(day: number): Promise<string> {
+    const input = await Deno.readTextFile(`./puzzleAssets/puzzleDay${day}.txt`);
+    return input.trim();
+  }
+  
+  /**
+   * Reads puzzle input from a file and returns it as an array of lines
+   */
+  export async function readInputLines(day: number): Promise<string[]> {
+    const input = await readInput(day);
+    return input.split('\n');
+  }
+  
+  /**
+   * Reads puzzle input from a file and returns it as an array of numbers
+   */
+  export async function readInputNumbers(day: number): Promise<number[]> {
+    const lines = await readInputLines(day);
+    return lines.map(Number);
+  }
